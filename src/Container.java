45c2426c7085974e6584bd693e3d04ae2b610fe2
sam0s <samiscoolco@gmail.com>
2020-02-14 17:22:58 -0500

    VERSION 1.0.0

--- src/Container.java
+++ src/Container.java
@@ -7,6 +7,10 @@ public class Container {
 	int x;
 	int y;
 	int color;
+	
+	Color outer = new Color(255,0,0);
+	Color inner = new Color(255,255,255,50);
+	
 	public Container(int sizex,int sizey,int x,int y){
 		this.sizex=sizex;
 		this.sizey=sizey;
@@ -16,7 +20,9 @@ public class Container {
 	}
 	public void draw(Graphics surface){
 		//surface.fillRect(arg0, arg1, arg2, arg3, arg4, arg5, arg6);
-		surface.setColor(Color.orange);
+		//surface.setColor(inner);
+		surface.fillRect(x, y, sizex, sizey);
+		//surface.setColor(outer);
 		surface.drawRect(x, y, sizex, sizey);
 		
 	}
--- src/Game.java
+++ src/Game.java
@@ -1,43 +1,81 @@
+import java.awt.FontFormatException;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.newdawn.slick.*;
 
-public class Game extends BasicGame 
+public class Game extends BasicGame
 {
 	Image i;
 	Container menu;
-	
+	Input input;
+	Font f;
+
 	public Game(String gamename)
 	{
 		super(gamename);
 	}
 
-	@Override
-	public void init(GameContainer gc) throws SlickException {
-		i = new Image("img.png");
-		menu = new Container(500,64,200,500);
+	public int[] getMouse()
+	{
+		return new int[]
+		{ input.getMouseX(), input.getMouseY() };
 	}
 
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {
-		
-		//just a test
-		if (menu.y>300){
-			menu.y-=0.05*(menu.y-300);
+	public void init(GameContainer gc) throws SlickException
+	{
+		i = new Image("hogBoss.jpg");
+		input = gc.getInput();
+		menu = new Container(500, 64, 200, 500);
+		java.awt.Font fontRaw = null;
+		try
+		{
+			fontRaw = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, new java.io.File("FreeMonoBold.ttf"));
+			fontRaw = fontRaw.deriveFont(32f);
+		} catch (FontFormatException e)
+		{
+			e.printStackTrace();
+
+		} catch (IOException e)
+		{
+			e.printStackTrace();
 		}
+		if (fontRaw == null)
+		{
+			fontRaw = new java.awt.Font("Default", 0, 28);
+		}
+		f = new TrueTypeFont(fontRaw, true);
+	}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException
+	{
+
+		gc.getGraphics().setFont(f);
+		System.out.println(i);
+		// just a test
+		if (menu.y > 300)
+		{
+			menu.y -= 0.07 * (menu.y - 300);
+		}
+
 	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
+		i.draw(32, 32, 200,200);
+		f.drawString(32, 32, String.format("Coomer Shelter (%d, %d)", getMouse()[0], getMouse()[1]), Color.orange);
 		menu.draw(g);
-		//i.draw(32,32,300,300);
 	}
 
-	public void loop(){
-		
+	public void loop()
+	{
+
 	}
-	
+
 	public static void main(String[] args)
 	{
 		try
@@ -46,8 +84,8 @@ public class Game extends BasicGame
 			appgc = new AppGameContainer(new Game("COOMER SHELTER"));
 			appgc.setDisplayMode(640, 480, false);
 			appgc.start();
-		}
-		catch (SlickException ex)
+
+		} catch (SlickException ex)
 		{
 			Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
 		}
