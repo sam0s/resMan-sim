c095ff017644e395d4055683a4807c14e98c4704
jdedmondt <jdedmondt@gmail.com>
2020-04-04 23:25:50 -0400

    cleaning up resource system. removing bad implementation of production rate management

--- src/Resources.java
+++ src/Resources.java
@@ -11,11 +11,7 @@ public class Resources {
 	 */
 	int raw_res[]; 
 	
-	float power_io[]; /* power production and usage rates */
-	float food_io[]; /* food production and usage rates */
-	float water_io[]; /* water production and usage rates */
-	
-	int staff;
+	int n_staff;
 	
 	Vector<Human> staff_list;
 	
@@ -25,10 +21,7 @@ public class Resources {
 		this.power_store = 0;
 		this.food_store = 0;
 		this.water_store = 0;
-		this.power_io = new float[2];
-		this.food_io = new float[2];
-		this.water_io = new float[2];
-		this.staff = 0;
+		this.n_staff = 0;
 		this.money = 0;
 		
 		this.raw_res = new int[10];
@@ -39,21 +32,16 @@ public class Resources {
 	}
 	
 	public void update(int delta) {
-		power_store += power_io[0]-power_io[1];
-		food_store += food_io[0]-food_io[1];
-		water_store += water_io[0]-water_io[1];
+		
 	}
 	
-	public void calc_rates() {
-		// loop through all rooms, add power gen/use rates of each.
-		// loop through all rooms, add water gen/use rates of each.
-		// add water usage by water usage per person * staff number.
-		// loop through all rooms, add food usage/use rates of each.
-		// add food usage by food usage per person * staff number.
-		
-		// for now
-		power_io[0] = power_io[1] = 1; // MWe
-		food_io[0] = food_io[1] = 0.25f; // tons
-		water_io[0] = water_io[1] = 10; // liters 
+	public void add_staff(Human staff) {
+		staff_list.addElement(staff);
+		n_staff++;
+	}
+	
+	public void remove_staff(Human staff) {
+		staff_list.remove(staff);
+		n_staff--;
 	}
 }
